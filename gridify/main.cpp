697cf797b82255d7a1c1b71cc302800f0bcfaed5
Bruno Manganelli <bruno.manga95@gmail.com>
2021-04-22 17:11:20 +0200

    Parse entire PDB, pass in the atom ids of the binding site

--- gridify/main.cpp
+++ gridify/main.cpp
@@ -12,11 +12,10 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/Side_of_triangle_mesh.h>
+#include <CGAL/Triangulation_3.h>
 #include <CGAL/algorithm.h>
 #include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
 #include <CGAL/convex_hull_3.h>
-#include <CGAL/Triangulation_3.h>
-
 
 using K = CGAL::Exact_predicates_inexact_constructions_kernel;
 using Polyhedron_3 = CGAL::Polyhedron_3<K>;
@@ -25,7 +24,6 @@ using Primitive = CGAL::AABB_face_graph_triangle_primitive<Polyhedron_3>;
 using Traits = CGAL::AABB_traits<K, Primitive>;
 using Tree = CGAL::AABB_tree<Traits>;
 using Point_inside = CGAL::Side_of_triangle_mesh<Polyhedron_3, K>;
-
 
 struct bounds {
   static constexpr std::pair<float, float> INIT = {
@@ -50,29 +48,66 @@ struct points_checker {
   Point_inside inside_tester;
 };
 
-std::pair<std::vector<Point_3>, bounds> parse_pdb(std::ifstream &ifs)
+struct pdb_entry {
+  Point_3 pos;
+  std::string residue;
+  std::string atom;
+  int residue_id;
+};
+
+struct pdb {
+  std::vector<pdb_entry> atoms;
+};
+
+pdb parse_pdb(std::ifstream &ifs)
 {
-  std::vector<Point_3> points;
+  pdb result;
   bounds bounds;
   std::string str;
 
   while (std::getline(ifs, str)) {
     std::istringstream iss(str);
-    std::string unused;
-    // NOTE: We could read lines at specific offsets, but I think this is more robust.
-    iss >> unused >> unused >> unused >> unused >> unused;
+    std::string unused, atom, residue;
+    int residue_id;
+    // NOTE: We could read lines at specific offsets, but I think this is more
+    // robust.
+    iss >> unused >> unused >> atom >> residue;
+    iss >> residue_id;
+    if (iss.fail()) {
+      iss.clear();
+      iss >> unused;
+      iss >> residue_id;
+    }
+
     float x, y, z;
     iss >> x >> y >> z;
-    points.emplace_back(x, y, z);
-    bounds.x.first = std::min(bounds.x.first, x);
-    bounds.x.second = std::max(bounds.x.second, x);
-
-    bounds.y.first = std::min(bounds.y.first, y);
-    bounds.y.second = std::max(bounds.y.second, y);
-
-    bounds.z.first = std::min(bounds.z.first, z);
-    bounds.z.second = std::max(bounds.z.second, z);
+    auto entry = pdb_entry{{x, y, z}, residue, atom, residue_id};
+    result.atoms.push_back(std::move(entry));
   }
+  return result;
+}
+
+std::pair<std::vector<Point_3>, bounds> get_binding_site(
+    const pdb &pdb,
+    const std::vector<int> &atoms)
+{
+  std::vector<Point_3> points;
+  bounds bounds;
+
+  for (int id : atoms) {
+    const auto &pos = pdb.atoms[id - 1].pos;
+    points.push_back(pos);
+
+    bounds.x.first = std::min<float>(bounds.x.first, pos.x());
+    bounds.x.second = std::max<float>(bounds.x.second, pos.x());
+
+    bounds.y.first = std::min<float>(bounds.y.first, pos.y());
+    bounds.y.second = std::max<float>(bounds.y.second, pos.y());
+
+    bounds.z.first = std::min<float>(bounds.z.first, pos.z());
+    bounds.z.second = std::max<float>(bounds.z.second, pos.z());
+  }
+
   return {points, bounds};
 }
 
@@ -95,7 +130,10 @@ void for_point_in_poly(const Polyhedron_3 &poly,
   }
 }
 
-int gen_grid_pdb(std::ostream &out, const Polyhedron_3 &poly, const bounds &bounds, float spacing)
+int gen_grid_pdb(std::ostream &out,
+                 const Polyhedron_3 &poly,
+                 const bounds &bounds,
+                 float spacing)
 {
   out << "CRYST1    0.000    0.000    0.000  90.00  90.00  90.00 P 1           "
          "1\n";
@@ -115,15 +153,18 @@ int gen_grid_pdb(std::ostream &out, const Polyhedron_3 &poly, const bounds &boun
 
 int main(int argc, char *argv[])
 {
-  auto opts = cxxopts::Options("gridify", "Generates a grid of points within the convex hull of the input points");
+  auto opts = cxxopts::Options(
+      "gridify",
+      "Generates a grid of points within the convex hull of the input points");
 
   // clang-format off
   opts.add_options()("i,in_file", "input file", cxxopts::value<std::string>())
     ("o,out_file", "output file (stdout if omitted)",cxxopts::value<std::string>()->default_value(""))
     ("s,spacing", "grid spacing", cxxopts::value<float>()->default_value("1.0f"))
-    ("v,verbose", "display extra information", cxxopts::value<bool>()->default_value("false"));
+    ("v,verbose", "display extra information", cxxopts::value<bool>()->default_value("false"))
+    ("a,site_atoms", "list of atom ids that make up the binding site", cxxopts::value<std::vector<int>>());
   // clang-format on
- 
+
   opts.parse_positional("in_file");
 
   auto parsed_opts = opts.parse(argc, argv);
@@ -134,11 +175,17 @@ int main(int argc, char *argv[])
     return -1;
   }
 
+  if (parsed_opts.count("site_atoms") == 0) {
+    std::cerr << "ERROR: You must specify the atoms of the binding site\n";
+    std::cout << opts.help();
+    return -1;
+  }
+
   auto in_file = parsed_opts["in_file"].as<std::string>();
   auto out_file = parsed_opts["out_file"].as<std::string>();
   auto spacing = parsed_opts["spacing"].as<float>();
   bool verbose = parsed_opts["verbose"].as<bool>();
-
+  auto atoms = parsed_opts["site_atoms"].as<std::vector<int>>();
 
   auto pdb_file = std::ifstream(in_file);
   if (!pdb_file.is_open()) {
@@ -146,20 +193,26 @@ int main(int argc, char *argv[])
     return -1;
   }
 
-  auto [points, bounds] = parse_pdb(pdb_file);
+  auto pdb = parse_pdb(pdb_file);
+  auto [points, bounds] = get_binding_site(pdb, atoms);
 
   if (verbose) {
-    std::cout << fmt::format("Parsed pdb file '{}': {} points\n", in_file,
-                             points.size());
+    std::cout << fmt::format("Parsed pdb file '{}': {} atoms\n", in_file,
+                             pdb.atoms.size());
+    std::cout << fmt::format("Binding site info: Num residues {}, : {} atoms\n",
+                             in_file, pdb.atoms.size());
     std::cout << fmt::format(R"(
+--- Binding site info ---
+Number of atoms specified: {}
 Bounds are: 
      MIN     MAX
 x    {:.3f}  {:.3f}
 y    {:.3f}  {:.3f}
 z    {:.3f}  {:.3f}
 )",
-                             bounds.x.first, bounds.x.second, bounds.y.first,
-                             bounds.y.second, bounds.z.first, bounds.z.second);
+                             points.size(), bounds.x.first, bounds.x.second,
+                             bounds.y.first, bounds.y.second, bounds.z.first,
+                             bounds.z.second);
   }
 
   Polyhedron_3 poly;
@@ -176,7 +229,9 @@ z    {:.3f}  {:.3f}
   }
 
   if (verbose) {
-    std::cout << fmt::format("Generated grid to output '{}': {} grid points with spacing of {:.4f}\n",
-                             out_file, num_grid_points, spacing);
+    std::cout << fmt::format(
+        "Generated grid to output '{}': {} grid points with spacing of "
+        "{:.4f}\n",
+        out_file, num_grid_points, spacing);
   }
 }
